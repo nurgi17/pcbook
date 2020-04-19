44e93e2ae366c770b0287916cd53e7b1f9f498c2
nurgi17 <usen.nurgisa@gmail.com>
2020-04-19 17:28:09 +0600

    Implementation of Unary grpc service with client and testing

---
+++ Example_01/service/laptop_store.go
@@ -0,0 +1,62 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"github.com/jinzhu/copier"
+	"github.com/nurgi17/pcbook/pb"
+	"sync"
+)
+
+var ErrAlreadyExists = errors.New("record already exists")
+
+type LaptopStore interface {
+	Save (laptop *pb.Laptop) error
+	Find(id string) (*pb.Laptop, error)
+}
+
+type InMemoryLaptopStore struct {
+	mutex sync.RWMutex
+	data map[string]*pb.Laptop
+}
+
+func NewInMemoryLaptopStore() *InMemoryLaptopStore {
+	return &InMemoryLaptopStore{
+		data:   make(map[string]*pb.Laptop),
+	}
+}
+
+func (store*InMemoryLaptopStore) Save(laptop *pb.Laptop) error {
+	store.mutex.Lock()
+	defer store.mutex.Unlock()
+
+	if store.data[laptop.Id] != nil {
+		return ErrAlreadyExists
+	}
+
+	other := &pb.Laptop{}
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+	store.data[other.Id] = other
+	return nil
+}
+
+func (store *InMemoryLaptopStore)Find(id string) (*pb.Laptop, error) {
+	store.mutex.RLock()
+	defer store.mutex.RUnlock()
+
+	laptop := store.data[id]
+	if laptop == nil {
+		return nil, nil
+	}
+
+	//deep copy
+	other := &pb.Laptop{}
+	err := copier.Copy(other, laptop)
+	if err != nil {
+		return nil, fmt.Errorf("cannot copy laptop data: %w", err)
+	}
+	return other, nil
+}
